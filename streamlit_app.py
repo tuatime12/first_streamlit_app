21779cbcbf3b4988d5ecab861945d4b555485827
tuatime12 <149432403+tuatime12@users.noreply.github.com>
2023-10-30 14:24:31 -0600

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -12,3 +14,10 @@ streamlit.text(' 🥑🍞 Avacado Toast')
 
  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+#import all packages, create a Pandas DF
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
