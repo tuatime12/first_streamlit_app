e212c23dde86d0111899a75d8390ea4f32aa9c07
tuatime12 <149432403+tuatime12@users.noreply.github.com>
2023-11-02 13:12:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ streamlit.dataframe(fruits_to_show)
 
 #create API to get fruityvice data
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+#streamlit.text(fruityvice_response.json())
 
 
 streamlit.header("Fruityvice Fruit Advice!")
