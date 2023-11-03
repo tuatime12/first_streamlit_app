6f36664e55ef1c0ed51fb76c640d0b7a943b1427
tuatime12 <149432403+tuatime12@users.noreply.github.com>
2023-11-03 12:15:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,14 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # makes adataframe to show normalized json
 streamlit.dataframe(fruityvice_normalized)
+
+
+
+## Query Trial Account data in Snowflake
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
